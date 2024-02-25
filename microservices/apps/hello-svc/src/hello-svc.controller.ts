369802e1889b6aebd43598f73d30f14f15afda07
Fi44er <vaceslavmalugin81@gmail.com>
2024-02-25 21:32:38 +0500

    update nest cli and folder proto structure

--- microservices/apps/hello-svc/src/hello-svc.controller.ts
+++ microservices/apps/hello-svc/src/hello-svc.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { HelloSvcService } from './hello-svc.service';
-import { HelloWorldResponse } from '../../../proto/hello';
+
+import { HelloWorldResponse } from '../../../proto/hello/hello';
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class HelloSvcController {
   constructor(private readonly helloSvcService: HelloSvcService) {}
 
+  @GrpcMethod('GetHello', 'HelloWorld')
   @Get()
   getHello(name: HelloWorldResponse): HelloWorldResponse {
     return this.helloSvcService.getHello(name);
